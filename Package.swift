a4df4a05a8608c326debf07712bca530e36ad257
alessio diimperio <alessio.diimperio@booli.se>
2024-06-13 18:52:47 +0200

    update SPM

--- Package.swift
+++ Package.swift
@@ -1,5 +1,21 @@
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
-  name: "Locksmith"
+    name: "Locksmith",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "Locksmith",
+            targets: ["Locksmith"]),
+    ],
+    targets: [
+        .target(
+            name: "Locksmith",
+            path: "Locksmith",
+            exclude: ["Info.plist"]
+        )
+    ]
 )
